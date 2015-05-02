f20e4dc5889a01469bed831b80b89388e1cb60cb
Wylie Standage-Beier <thewyliestcoyote@gmail.com>
2015-05-02 13:58:57 -0700

    created helloworld in C and python

---
+++ helloworld.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+
+// This is a simple hello world program in C, Hello World!
+int main()
+{
+    printf("Hello World!");
+    return 0;
+}
---
+++ helloworld.py
@@ -0,0 +1,6 @@
+
+# This is a simple hello world program in python
+
+if __name__ == '__main__':
+    print('Hello World!')
+
